34fe8efac3ea4a5dc7892ed2847e36b91e3c234f
Konstantin <3213312@gmail.comm>
2023-02-11 11:53:49 +0300

    added 3 homeworks

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.Write("Введите трехзначное число: ");
+int.TryParse(Console.ReadLine(), out int number);
+int a = (number / 10);
+int b = (a % 10);
+Console.WriteLine( b );
+
+
+// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+/*
+Console.Write("Введите число: ");
+string s = Console.ReadLine();
+int a = Convert.ToInt32(s);
+
+int l = s.Length;
+
+    if (l >= 3)
+    {
+        Console.WriteLine("Третья цифра:" + s[2]);
+    }
+    else
+    {
+        Console.WriteLine("третьей цифры нет");
+    }
+
+
+
+
+
+
+
+//  Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+    if (number < 1|| number > 7)
+{
+    Console.WriteLine("Вы ввели неверное число");
+}
+else
+{
+    if (number > 5) 
+    Console.WriteLine("Да");
+
+else
+    Console.WriteLine("Нет");
+}
+*/
